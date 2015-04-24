ec69014194de75c34676152dc67430f9687935fe
Ryan Wing <ryanwing@earthlink.net>
2015-04-24 12:26:41 -0600

    Initial semi-working version of socket-based chat.

---
+++ Android/app/src/main/java/com/konacode/courseexplorer/ConversationActivity.java
@@ -0,0 +1,300 @@
+package com.konacode.courseexplorer;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ConversationActivity extends Activity
+{
+   String mAddress;
+   int mPort;
+   EditText mMessageEdit;
+   TextView mTextView;
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState)
+   {
+      super.onCreate(savedInstanceState);
+
+      setContentView(R.layout.activity_conversation);
+      getActionBar().setDisplayHomeAsUpEnabled(true);
+
+      Bundle extras = getIntent().getExtras();
+
+      if(extras != null)
+      {
+         mAddress = extras.getString("address");
+         mPort = extras.getInt("port");
+      }
+
+      mMessageEdit = (EditText)findViewById(R.id.chat_edit_id);
+      mTextView = (TextView)findViewById(R.id.chat_response_view_id);
+
+      ReceiveTask task = new ReceiveTask(mAddress, mPort);
+      task.execute();
+
+      Button send = (Button) findViewById(R.id.chat_send_button_id);
+
+      send.setOnClickListener(new View.OnClickListener()
+      {
+         @Override
+         public void onClick(View pView)
+         {
+            String message = mMessageEdit.getText().toString();
+            SendTask task = new SendTask(mAddress, mPort, message);
+
+            task.execute();
+         }
+      });
+   }
+
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu)
+   {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.menu_conversation, menu);
+      return true;
+   }
+
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item)
+   {
+      Context context = this;
+      Intent intent = null;
+      boolean result = true;
+
+      // Handle action bar item clicks here. The action bar will
+      // automatically handle clicks on the Home/Up button, so long
+      // as you specify a parent activity in AndroidManifest.xml.
+      int id = item.getItemId();
+
+      switch(id)
+      {
+         case android.R.id.home:
+            /*
+            When we go back (home) from the action bar, we start a new intent here
+            for the main activity.
+             */
+            intent = new Intent(context, ChatActivity.class);
+            break;
+         case R.id.action_settings:
+            Toast.makeText(context, "There are no settings defined yet!", Toast.LENGTH_LONG).show();
+            break;
+         default:
+            result = super.onOptionsItemSelected(item);
+            break;
+      }
+
+      if(intent != null)
+      {
+         startActivity(intent);
+      }
+
+      return result;
+   }
+
+   public class ReceiveTask extends AsyncTask<Void, Void, Void>
+   {
+      String mAddress;
+      int mPort;
+
+      ReceiveTask(String pAddress, int pPort)
+      {
+         mAddress = pAddress;
+         mPort = pPort;
+      }
+
+      @Override
+      protected Void doInBackground(Void... pArguments)
+      {
+         InetAddress server = null;
+         Socket socket = null;
+
+         try
+         {
+            server = InetAddress.getByName(mAddress);
+            socket = new Socket(server, mPort);
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+            byte[] buffer = new byte[1024];
+            int bytesRead = 0;
+
+            while(socket.isConnected())
+            {
+               String response = "";
+               InputStream inputStream = socket.getInputStream();
+
+               // inputStream.read() will block if no data return
+               for(bytesRead = 0; (bytesRead = inputStream.read(buffer)) != -1; )
+               {
+                  byteArrayOutputStream.write(buffer, 0, bytesRead);
+
+                  response += byteArrayOutputStream.toString("UTF-8");
+               }
+
+               mTextView.append(response);
+            }
+         }
+         catch(UnknownHostException pException)
+         {
+            pException.printStackTrace();
+
+            Log.i("conversation", "UnknownHostException: " + pException.toString());
+         }
+         catch(IOException pException)
+         {
+            pException.printStackTrace();
+
+            Log.i("conversation", "IOException: " + pException.toString());
+         }
+         finally
+         {
+            if(socket != null)
+            {
+               try
+               {
+                  socket.close();
+               }
+               catch(IOException pException)
+               {
+                  pException.printStackTrace();
+               }
+            }
+         }
+
+         return null;
+      }
+
+      @Override
+      protected void onPostExecute(Void pResult)
+      {
+         super.onPostExecute(pResult);
+      }
+   }
+
+   public class SendTask extends AsyncTask<Void, Void, Void>
+   {
+      String mAddress;
+      int mPort;
+      String mMessage;
+
+      SendTask(String pAddress, int pPort, String pMessage)
+      {
+         mAddress = pAddress;
+         mPort = pPort;
+      }
+
+      @Override
+      protected Void doInBackground(Void... pArguments)
+      {
+         InetAddress server = null;
+         Socket socket = null;
+         BufferedReader in = null;
+         PrintWriter out = null;
+
+         try
+         {
+            server = InetAddress.getByName(mAddress);
+            socket = new Socket(server, mPort);
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+            byte[] buffer = new byte[1024];
+            int bytesRead = 0;
+
+            if(socket.isConnected())
+            {
+               mTextView.append(mMessage);
+
+               in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+               out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
+               out.write(mMessage);
+
+               String response = in.readLine();
+
+               mTextView.append(response);
+            }
+         }
+         catch(UnknownHostException pException)
+         {
+            pException.printStackTrace();
+
+            Log.i("conversation", "UnknownHostException: " + pException.toString());
+         }
+         catch(IOException pException)
+         {
+            pException.printStackTrace();
+
+            Log.i("conversation", "IOException: " + pException.toString());
+         }
+         finally
+         {
+            if(socket != null)
+            {
+               try
+               {
+                  socket.close();
+               }
+               catch(IOException pException)
+               {
+                  pException.printStackTrace();
+               }
+            }
+
+            if(in != null)
+            {
+               try
+               {
+                  in.close();
+               }
+               catch(IOException pException)
+               {
+                  pException.printStackTrace();
+               }
+            }
+
+            if(out != null)
+            {
+               try
+               {
+                  out.close();
+               }
+               catch(Exception pException)
+               {
+                  pException.printStackTrace();
+               }
+            }
+         }
+
+         return null;
+      }
+
+      @Override
+      protected void onPostExecute(Void pResult)
+      {
+         super.onPostExecute(pResult);
+      }
+   }
+}
